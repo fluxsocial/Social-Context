85164a5cda148de04a530820f3adc5021b262d4a
Kaicho Sun <kaichaosuna@gmail.com>
2021-12-06 11:49:15 +0800

    more fix

--- release.sh
+++ release.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 if ! nix-shell --help &> /dev/null
 then
     echo "nix-shell could not be found! Are you sure it is installed correctly?"
@@ -14,7 +17,7 @@ echo "Create release with full index & no signals or time index..."
 cp ./hc-dna/workdir/dna_basic_full_index.yaml ./hc-dna/workdir/dna.yaml
 cd ./hc-dna && ./build.sh
 cd ../
-npm run build
+npm install && npm run build
 
 #Check if full_index directory exists, if not create
 [ ! -d "./release/full_index" ] && mkdir "./release/full_index"
@@ -31,7 +34,7 @@ echo "Create release with full index + signals + time index..."
 cp ./hc-dna/workdir/dna_signals.yaml ./hc-dna/workdir/dna.yaml
 cd ./hc-dna && ./build.sh
 cd ../
-npm run build
+npm install && npm run build
 
 #Create the signal release dir
 [ ! -d "./release/signal" ] && mkdir "./release/signal"
@@ -47,7 +50,7 @@ echo "Create release with full index + time index but no signals..."
 cp ./hc-dna/workdir/dna_time_index.yaml ./hc-dna/workdir/dna.yaml
 cd ./hc-dna && ./build.sh
 cd ../
-npm run build
+npm install && npm run build
 
 [ ! -d "./release/full_time_index" ] && mkdir "./release/full_time_index"
 
